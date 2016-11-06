6ea13f8124fb7b6aec2ff2bf2154d1b78c9ae9f7
Pineapple <Pineapple@DESKTOP-3R2UQV1>
2016-11-05 23:56:47 -0400

    completed Bug3.

--- Assignment2/src/ca/utoronto/utm/paint/PaintPanel.java
+++ Assignment2/src/ca/utoronto/utm/paint/PaintPanel.java
@@ -16,13 +16,15 @@ class PaintPanel extends JPanel implements Observer, MouseMotionListener, MouseL
 	private int i=0;
 	private PaintModel model; // slight departure from MVC, because of the way painting works
 	private View view; // So we can talk to our parent or other components of the view
-
+	
+	private String state;
 	private modeStrategy mode;
 	private Circle circle; // the circle we are building
 	private Rectangle rectangle;
 	private Square square;
 	private Point point; //<---- Find out what this value actually does
 	
+	private modeStrategy drawLine, drawCircle, drawRectangle, drawSquare;
 	
 	public PaintPanel(PaintModel model, View view){
 		this.setBackground(Color.white);
@@ -32,6 +34,11 @@ class PaintPanel extends JPanel implements Observer, MouseMotionListener, MouseL
 		this.model = model;
 		this.model.addObserver(this);
 		this.view=view;
+		
+		this.drawLine = new createSquiggle();
+		this.drawCircle = new createCircle();
+		this.drawRectangle = new createRectangle();
+		this.drawSquare = new createSquare();
 	}
 
 	/**
@@ -40,74 +47,16 @@ class PaintPanel extends JPanel implements Observer, MouseMotionListener, MouseL
 	public void paintComponent(Graphics g) {
 		// Use g to draw on the JPanel, lookup java.awt.Graphics in
 		// the javadoc to see more of what this can do for you!!
-		
         super.paintComponent(g); //paint background
         Graphics2D g2d = (Graphics2D) g; // lets use the advanced api
 		// setBackground (Color.blue); 
 		// Origin is at the top left of the window 50 over, 75 down
 		g2d.setColor(Color.black);
 
-		// Draw Lines
-		ArrayList<Point> points = this.model.getPoints();
-		for(int i=0;i<points.size()-1; i++){
-			Point p1=points.get(i);
-			Point p2=points.get(i+1);
-			g2d.drawLine(p1.getX(), p1.getY(), p2.getX(), p2.getY());
-		}
-		
-		// Draw Circles
-		ArrayList<Circle> circles = this.model.getCircles();
-		for(Circle c: this.model.getCircles()){
-			int radius = c.getRadius();
-			int x = (c.getOrigin().getX()-radius);
-			int y = (c.getOrigin().getY()-radius);
-			g2d.drawOval(x, y, radius*2, radius*2);
-		}
-		
-		// Draw Rectangles
-		ArrayList<Rectangle> rectangles = this.model.getRectangles();
-		for(Rectangle r: this.model.getRectangles()){
-			int width = r.getWidth();
-			int height = r.getHeight();
-			int x,y;
-			
-			//Cases for where the top left coordinates of the rectangle will start based on if the user is trying to drag to the right or left and up or down
-			if (r.getOrigin().getX() < r.getEnd().getX()) {
-				x = r.getOrigin().getX();
-			}
-			else {
-				x = r.getEnd().getX();
-			}
-			if (r.getOrigin().getY() < r.getEnd().getY()) {
-				y = r.getOrigin().getY();
-			}
-			else {
-				y = r.getEnd().getY();
-			}
-			g2d.drawRect(x, y, width, height);
-		}
-		
-		// Draw Squares
-		ArrayList<Square> square = this.model.getSquares();
-		for(Square s: this.model.getSquares()){
-			int side = s.getHeight();
-			int x,y;
-			
-			//Cases for where the top left coordinates of the square will start based on if the user is trying to drag to the right or left and up or down
-			if (s.getOrigin().getX() < s.getEnd().getX()) {
-				x = s.getOrigin().getX();
-			}
-			else {
-				x = s.getOrigin().getX() - s.getHeight();
-			}
-			if (s.getOrigin().getY() < s.getEnd().getY()) {
-				y = s.getOrigin().getY();
-			}
-			else {
-				y = s.getOrigin().getY() - s.getHeight();
-			}
-			g2d.drawRect(x, y, side, side);
-		}
+		this.drawLine.draw(this, g2d);
+		this.drawCircle.draw(this, g2d);
+		this.drawRectangle.draw(this, g2d);
+		this.drawSquare.draw(this, g2d);
 		g2d.dispose();
 	}
 
@@ -133,6 +82,7 @@ class PaintPanel extends JPanel implements Observer, MouseMotionListener, MouseL
 		else if (current_mode == "Square"){
 			this.mode= new createSquare();
 		}
+		this.state = current_mode;
 	}
 	// MouseMotionListener below
 	@Override
